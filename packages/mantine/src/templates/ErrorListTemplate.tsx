516181861fcf4e7a1fe7fdeb3bb6c69c94bce09f
yuki-js <me@aoki.app>
2024-01-13 01:59:17 +0900

    Fix base template, which was for corporate at the first place

--- packages/mantine/src/templates/ErrorListTemplate.tsx
+++ packages/mantine/src/templates/ErrorListTemplate.tsx
@@ -1,4 +1,4 @@
-import { Alert, List, Spoiler } from '@mantine/core';
+import { Alert, List } from '@mantine/core';
 import { ErrorListProps, FormContextType, RJSFSchema, StrictRJSFSchema, TranslatableString } from '@rjsf/utils';
 import { IconZoomExclamation } from '@tabler/icons-react';
 
@@ -13,13 +13,11 @@ export default function ErrorList<T = any, S extends StrictRJSFSchema = RJSFSche
   const { translateString } = registry;
   return (
     <Alert title={translateString(TranslatableString.ErrorsLabel)} icon={<IconZoomExclamation />}>
-      <Spoiler maxHeight={120} showLabel='Show more' hideLabel='Hide'>
-        <List size='sm'>
-          {errors.map((error, index) => (
-            <List.Item key={`error-${index}`}>{error.stack}</List.Item>
-          ))}
-        </List>
-      </Spoiler>
+      <List size='sm'>
+        {errors.map((error, index) => (
+          <List.Item key={`error-${index}`}>{error.stack}</List.Item>
+        ))}
+      </List>
     </Alert>
   );
 }
