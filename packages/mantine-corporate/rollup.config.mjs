7f1b18679790eafe5b0fe837ac5a8da6382cce4e
yuki-js <me@aoki.app>
2024-01-29 13:48:19 +0900

    add commonjs, banner

--- packages/mantine-corporate/rollup.config.mjs
+++ packages/mantine-corporate/rollup.config.mjs
@@ -1,9 +1,13 @@
 import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import banner from 'rollup-plugin-banner2';
 import path from 'path';
 
 const getPath = (storyPath) => path.resolve(process.cwd(), storyPath).replace(/\\/g, '/');
+
+const ROLLUP_EXCLUDE_USE_CLIENT = ['index'];
 
 export default {
   input: 'src/index.ts', // Adjust this to your entry point
@@ -27,6 +31,7 @@ export default {
   ],
   plugins: [
     resolve(),
+    commonjs(),
     esbuild({
       sourceMap: false,
       tsconfig: getPath('tsconfig.json'),
@@ -34,6 +39,13 @@ export default {
     postcss({
       extract: true,
       minimize: true,
+    }),
+    banner((chunk) => {
+      if (!ROLLUP_EXCLUDE_USE_CLIENT.includes(chunk.fileName)) {
+        return "'use client';\n";
+      }
+
+      return undefined;
     }),
   ],
 };
