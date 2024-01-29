fba3910f7f4ce5ab00d8c9c8d1952875846409a0
yuki-js <me@aoki.app>
2024-01-29 14:30:33 +0900

    build succeeded

--- packages/mantine-corporate/rollup.config.mjs
+++ packages/mantine-corporate/rollup.config.mjs
@@ -1,51 +1,37 @@
-import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import postcss from 'rollup-plugin-postcss';
-import commonjs from '@rollup/plugin-commonjs';
-import banner from 'rollup-plugin-banner2';
 import path from 'path';
 
 const getPath = (storyPath) => path.resolve(process.cwd(), storyPath).replace(/\\/g, '/');
-
-const ROLLUP_EXCLUDE_USE_CLIENT = ['index'];
 
 export default {
   input: 'src/index.ts', // Adjust this to your entry point
   output: [
     {
-      file: 'dist/index.js',
+      entryFileNames: '[name].cjs',
+      dir: 'dist',
       format: 'cjs',
       sourcemap: true,
+      exports: 'named',
+      preserveModules: true,
     },
     {
-      file: 'lib/index.js',
+      entryFileNames: '[name].mjs',
       format: 'esm',
+      dir: 'lib',
       sourcemap: true,
-    },
-    {
-      file: 'dist/mantine.umd.js',
-      name: '@aokiapp/rjsf-mantine-theme', // UMD name
-      format: 'umd',
-      sourcemap: true,
+      exports: 'named',
+      preserveModules: true,
     },
   ],
   plugins: [
-    resolve(),
-    commonjs(),
     esbuild({
       sourceMap: false,
       tsconfig: getPath('tsconfig.json'),
     }),
     postcss({
-      extract: true,
-      minimize: true,
-    }),
-    banner((chunk) => {
-      if (!ROLLUP_EXCLUDE_USE_CLIENT.includes(chunk.fileName)) {
-        return "'use client';\n";
-      }
-
-      return undefined;
+      modules: true,
+      extract: 'styles.css',
     }),
   ],
 };
